30bfb553daa7028caf0c4079e63e59463d7fd6c3
Utiwari1999 <utkarshtiwari95@gmail.com>
2020-05-17 12:18:10 +0530

    Login updated

--- app.js
+++ app.js
@@ -1,15 +1,57 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 const request = require('request');
 var ip_data ='125.19.12.216';
 
+//mongodb packages
+var mongoose = require("mongoose");
+
+//passport packages
+var LocalStrategy = require("passport-local");
+var passport = require("passport");
+var passportLocalMongoose = require("passport-local-mongoose");
+
+//importing user model
+var User = require("./models/user");
+
+//encryting password
+app.use(require("express-session")({
+  secret: "kuch bhi likh do",
+  resave: false,
+  saveUninitialized: false
+}));
+
+
+//Connecting to mongodb server and storing ip_data
+mongoose.connect("mongodb://localhost:27017/uson_mechanics", { useNewUrlParser: true,useUnifiedTopology: true });
+
+//body parser import
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
 app.use( express.static( "public" ));
+
+//Passport Configuration
+app.use(passport.initialize());
+app.use(passport.session());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+passport.use(new LocalStrategy(User.authenticate()));
+
+//passing current user to every route
+app.use(function(req, res, next){
+  res.locals.currentUser = req.user;
+  next();
+});
+
+//landing page route
 app.get("/", function(req, res){
   res.render("index.ejs");
   // res.send("This is the main page");
 });
 
-app.get("/ip", function(req, res){
+//IP finder API Route
+app.get("/ip", isLoggedIn, function(req, res){
   request("http://api.ipstack.com/check?access_key=e93a0af9a29cc5c42ce47d86a479d92a", function(error, reponse, body){
     var data = JSON.parse(body);
     ip_data=data["ip"];
@@ -20,10 +62,12 @@ app.get("/ip", function(req, res){
 
 });
 
+//Login Route
 app.get("/login", function(req, res){
   res.render("login.ejs");
 })
 
+//Loation finder API Route
 app.get("/location", function(req, res){
   var options = {
     method: 'GET',
@@ -43,6 +87,55 @@ app.get("/location", function(req, res){
   });
 });
 
+//Auth Routes
+//Register Routes
+app.get("/register", function(req, res){
+  res.render("register");
+});
+
+app.post("/register", function(req, res){
+  req.body.username
+  req.body.password
+  var newUser = new User({username: req.body.username});
+  User.register(newUser, req.body.password, function(err, user){
+    if (err) {
+      console.log(err);
+      return res.render("register");
+    }
+    passport.authenticate("local")(req, res, function(){
+      res.redirect("/");
+    });
+  });
+});
+
+//Login Routes
+app.get("/login", function(req, res){
+  res.render("/login");
+});
+
+app.post("/login", passport.authenticate("local",
+  {
+    successRedirect: "/",
+    failureRedirect: "/login"
+  }), function(req, res){
+
+  });
+
+//logout Routes
+app.get("/logout", function(req, res){
+  req.logout();
+  res.redirect("/");
+});
+
+//middleware
+function isLoggedIn(req, res, next){
+  if(req.isAuthenticated()){
+    return next();
+  }
+  res.redirect("/login");
+}
+
+//PORT
 app.listen(3000, function(){
   console.log("Serving app on port 3000");
 });
